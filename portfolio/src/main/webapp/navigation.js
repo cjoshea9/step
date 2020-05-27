3bd7d1777eeb79aeb737cc623d5abbf233782e91
Cory O'Shea <cjoshea9@gmail.com>
2020-05-27 17:26:35 -0400

    Home page remodeling to make profile picture and brief introduction as well as navbar resizing

---
+++ portfolio/src/main/webapp/navigation.js
@@ -0,0 +1,24 @@
+document.getElementById("navbar").innerHTML = `
+  <div>
+      <header> 
+        <nav>
+          <div>
+            <ul id="navbar-div">
+              <li class="navbar-li">
+                <a href="index.html">Home</a> 
+              </li>
+              <li class="navbar-li">
+                <a href="resume.html">Resume</a> 
+              </li>
+              <li class="navbar-li">
+                <a href="photos.html">Photos</a>
+              </li>
+              <li class="navbar-li">
+                <a href="contact.html">Contact</a> 
+              </li>
+            </ul>    
+          </div>
+        </nav>
+      </header>
+    </div>
+`;
