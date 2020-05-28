ea474bd28b4d952d720da6c30d04e7fa4df7e972
Cory O'Shea <cjoshea9@gmail.com>
2020-05-28 15:20:56 -0400

    Adds fraternity part to homepage and fix style updates from PR

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -27,22 +27,23 @@ function addRandomGreeting() {
   greetingContainer.innerText = "I was born in Sydney, Australia.";
 }
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlide(slideIndex);
 
 function nextSlide(n) {
-  showSlide(slideIndex+=n);
+  slideIndex+=n
+  showSlide(slideIndex);
 }
 
 function showSlide(n) {
-  var slides = document.getElementsByClassName("slide");
+  let slides = document.getElementsByClassName("slide");
   if (n > slides.length) {
     slideIndex = 1
   }
   if (n < 1) {
     slideIndex = slides.length
   }
-  for (var i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
